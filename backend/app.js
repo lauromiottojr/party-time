04454a22db6fed488c9c4523b67cb5abc8b79f59
lauromiottojr <junior.miotto36@gmail.com>
2022-12-28 09:22:45 -0300

    Config service router

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,15 @@ const app = express()
 app.use(cors()) // permite a requisição na mesma máquina/domínio
 app.use(express.json())
 
+// conexão com DB
 const conn = require("./db/conn")
 
 conn()
+
+// Rotas
+const routes = require("./routes/router")
+
+app.use('/api', routes)
 
 app.listen(3000, function () {
     console.log('server on!')
